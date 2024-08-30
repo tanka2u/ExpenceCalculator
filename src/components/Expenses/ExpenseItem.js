89ecad510e12216146e2c7a2329070b3c83fee4a
tanka2u <ptanka123@gmail.com>
2024-08-30 21:26:17 +0200

    first commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,17 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+function ExpenseItem({ title, date, amount }) {
+  return (
+    <Card className="expense-item">
+      <div>
+        <ExpenseDate date={date} />
+      </div>
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+      </div>
+      <div className="expense-item__price">${amount}</div>
+    </Card>
+  );
+}
+export default ExpenseItem;
